ae1ef1e10452e12b5a47d82d60c45b96f276f383
ImTonyS <tony.villaverde@hotmail.com>
2024-06-18 22:03:03 -0600

    shadcn out

--- app/dashboard/_components/BarCard.js
+++ app/dashboard/_components/BarCard.js
@@ -42,7 +42,7 @@ export default function BarCard({ name, apellido, email }) {
               className="-ml-0.5 mr-1.5 h-5 w-5 text-gray-400"
               aria-hidden="true"
             />
-            <span>Email</span>
+            <span>Borrar</span>
           </button>
         </div>
       </div>
--- components/barberos/Dashboard.js
+++ components/barberos/Dashboard.js
@@ -3,21 +3,18 @@ import {
   Disclosure,
   DisclosureButton,
   DisclosurePanel,
-  Menu,
-  MenuButton,
-  MenuItem,
-  MenuItems,
-  Transition,
 } from "@headlessui/react";
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
 import Logo from "@/app/images/logo.png";
 import Image from "next/image";
 import Link from "next/link";
 import ButtonAccount from "../ButtonAccount";
+import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 const navigation = [
-  { name: "Dashboard", href: "#", current: true },
-  { name: "Team", href: "#", current: false },
+  { name: "Dashboard", href: "/dashboard", current: true },
+  { name: "Barberos", href: "/dashboard/barberos", current: false },
   { name: "Projects", href: "#", current: false },
   { name: "Calendar", href: "#", current: false },
 ];
@@ -27,6 +24,9 @@ function classNames(...classes) {
 }
 
 export default function Dashboard({ children }) {
+  const path = usePathname();
+  const [current, setCurrent] = useState("/dashboard");
+
   return (
     <>
       <Disclosure as="nav" className="bg-barber-red">
@@ -51,7 +51,6 @@ export default function Dashboard({ children }) {
                               : "text-zinc-50 hover:bg-[#DADADA] hover:text-gray-900",
                             "rounded-md px-3 py-2 text-sm font-medium"
                           )}
-                          aria-current={item.current ? "page" : undefined}
                         >
                           {item.name}
                         </a>
@@ -113,61 +112,11 @@ export default function Dashboard({ children }) {
                   Calendar
                 </DisclosureButton>
               </div>
-              <div className="border-t border-gray-700 pb-3 pt-4">
-                <div className="flex items-center px-5">
-                  <div className="flex-shrink-0">
-                    <img
-                      className="h-10 w-10 rounded-full"
-                      src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                      alt=""
-                    />
-                  </div>
-                  <div className="ml-3">
-                    <div className="text-base font-medium text-white">
-                      Tom Cook
-                    </div>
-                    <div className="text-sm font-medium text-gray-400">
-                      tom@example.com
-                    </div>
-                  </div>
-                  <button
-                    type="button"
-                    className="relative ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                  >
-                    <span className="absolute -inset-1.5" />
-                    <span className="sr-only">View notifications</span>
-                    <BellIcon className="h-6 w-6" aria-hidden="true" />
-                  </button>
-                </div>
-                <div className="mt-3 space-y-1 px-2">
-                  <DisclosureButton
-                    as="a"
-                    href="#"
-                    className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
-                  >
-                    Your Profile
-                  </DisclosureButton>
-                  <DisclosureButton
-                    as="a"
-                    href="#"
-                    className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
-                  >
-                    Settings
-                  </DisclosureButton>
-                  <DisclosureButton
-                    as="a"
-                    href="#"
-                    className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
-                  >
-                    Sign out
-                  </DisclosureButton>
-                </div>
-              </div>
             </DisclosurePanel>
           </>
         )}
       </Disclosure>
-      <div>{children}</div>
+      <div className="mt-10">{children}</div>
     </>
   );
 }
